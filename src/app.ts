46eadf2d65670f0db363d711e61a4b79eadc2879
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-04-29 17:13:15 +0100

    health check route

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,34 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
 import testRoutes from './routes/testRoutes';
+
 import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // Routes
 app.use('/api/test', testRoutes);
+
+// Health Check
+app.get('/api/health', (req: Request, res: Response) => {
+    const healthCheck = {
+        uptime: process.uptime(),
+        message: 'OK',
+        timeStamp: Date.now()
+    };
+
+    try {
+        res.send(healthCheck);
+    
+    } catch (error) {
+        healthCheck.message = error + "";
+        res.status(503).send(healthCheck);
+    }
+})
 
 // Error Handler
 app.use(errorHandler);

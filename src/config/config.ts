e699a88f689a6583ba4ff2d196b277ca7c186087
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-04-30 12:18:30 +0100

    mongodb connection

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,11 +5,13 @@ dotenv.config();
 interface Config {
     port: number;
     nodeEnv: string;
+    dbConnectionString: string;
 };
 
 const config: Config = {
     port: Number(process.env.PORT) || 3000,
-    nodeEnv: process.env.NODE_ENV || 'development'
+    nodeEnv: process.env.NODE_ENV || 'development',
+    dbConnectionString: process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/emailSenderLocalDB'
 };
 
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import app from './app';
 import config from './config/config';
+import { connectDB } from './db/connection';
 
-app.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`);
-});
+const startServer = async () => {
+  try {
+      await connectDB()
+      app.listen(config.port, () => console.log(`Server running on port ${config.port}`));
+  } catch (error) {
+      process.exit(1); // Stop the process if DB connection fails
+  }
+};
+
+startServer();

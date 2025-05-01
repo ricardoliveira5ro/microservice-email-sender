09110e7bbc96a01bce1934214f987bbe09b40f50
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-05-01 14:57:14 +0100

    eslint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    {
+        ignores: ['eslint.config.mjs','dist/**'],
+    },
+    eslint.configs.recommended,
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            semi: ['error', 'always'],                   // Require semicolons
+            'comma-dangle': ['error', 'always-multiline'], // Enforce trailing commas in multiline
+            'object-curly-spacing': ['error', 'always'], // Require spacing inside braces
+            'no-trailing-spaces': 'error',               // Disallow trailing whitespace
+            
+            '@typescript-eslint/explicit-function-return-type': ['warn'],
+            '@typescript-eslint/explicit-module-boundary-types': ['warn'],
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        },
+    },
+);

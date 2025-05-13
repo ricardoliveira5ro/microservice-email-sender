e53b048883d91f1abd609bd693966059361008df
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-05-13 16:49:45 +0100

    implement basic queue with redis and bullmq

--- src/api/app.ts
+++ src/api/app.ts
@@ -6,6 +6,9 @@ import userRoutes from './routes/user';
 import emailRoutes from './routes/email';
 
 import { errorHandler } from './middlewares/errorHandler';
+import { Job, Queue, Worker } from 'bullmq';
+import sendEmail from './services/email';
+import Redis from 'ioredis';
 
 const app = express();
 
@@ -40,6 +43,29 @@ app.use(express.static(staticPath));
 
 app.get(/(.*)/, (req: Request, res: Response) => {
   res.sendFile(path.join(staticPath, 'index.html'));
+});
+
+const connection = new Redis({ maxRetriesPerRequest: null });
+const jobQueue = new Queue('jobQueue', { connection });
+
+app.post('/api/add-job', async (req, res) => {
+    const job = await jobQueue.add('job', req.body);
+    res.send({ jobId: job.id });
+});
+
+// Worker to process jobs from the queue
+const worker = new Worker('jobQueue', async (job: Job) => {
+    const { recipients, subject, text, category } = job.data as { recipients: [], subject: string, text: string, category: string };
+    await sendEmail(recipients, subject, text, category);
+}, { connection });
+
+// Event listeners for worker
+worker.on('completed', (job: Job) => {
+  console.log(`Job ${job.id ?? 'unknown'} completed successfully`);
+});
+
+worker.on('failed', (job, err) => {
+  console.error(`Job ${job?.id ?? 'unknown'} failed with error ${err.message}`);
 });
 
 // Error Handler
